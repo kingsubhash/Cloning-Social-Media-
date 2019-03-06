2eddda896a489a1e902e56b1562ec8056c4abd30
Iam Subhash <kingsubhash12@gmail.com>
2019-03-06 11:48:58 +0545

    Adding to  git

---
+++ posts/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+#from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name='posts'
+
+urlpatterns = [
+    url(r"^$", views.PostList.as_view(), name="all"),
+    url(r"new/$", views.CreatePost.as_view(), name="create"),
+    url(r"by/(?P<username>[-\w]+)/$",views.UserPosts.as_view(),name="for_user"),
+    url(r"by/(?P<username>[-\w]+)/(?P<pk>\d+)/$",views.PostDetail.as_view(),name="single"),
+    url(r"delete/(?P<pk>\d+)/$",views.DeletePost.as_view(template_name='posts/post_conform_delete.html'),name="delete"),
+]
